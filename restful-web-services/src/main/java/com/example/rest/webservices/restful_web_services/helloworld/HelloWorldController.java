7e3e59bad65e14f5aaafdfa6a14fe2655300b40f
Nihaal <nayaknihaal04@gmail.com>
2026-06-02 22:38:03 +0530

    exception handling

--- restful-web-services/src/main/java/com/example/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/example/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.example.rest.webservices.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 
@@ -18,5 +19,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
