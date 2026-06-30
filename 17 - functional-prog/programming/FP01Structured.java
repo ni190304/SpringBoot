24e6ed75dee810a0f639f9101240e6552345d966
Nihaal <nayaknihaal04@gmail.com>
2026-06-30 20:05:49 +0530

    functional programming java

---
+++ 17 - functional-prog/programming/FP01Structured.java
@@ -0,0 +1,31 @@
+package programming;
+
+import java.util.List;
+
+public class FP01Structured {
+
+    public static void main(String[] args) {
+
+        printAllNumbersInListStructured(List.of(12, 9, 23, 32, 11));
+        printEvenNumbersInListStructured(List.of(12, 9, 23, 32, 11));
+
+    }
+
+    private static void printAllNumbersInListStructured(List<Integer> numbers) {
+
+        for (int integer : numbers) {
+            System.out.println(integer);
+        }
+
+    }
+
+    private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+
+        for (int integer : numbers) {
+            if (integer % 2 == 0) {
+                System.out.println(integer);
+            }
+        }
+
+    }
+}
---
+++ 17 - functional-prog/programming/FP02Functional.java
@@ -0,0 +1,52 @@
+package programming;
+
+import java.util.List;
+
+public class FP02Functional {
+
+    public static void main(String[] args) {
+
+        printAllNumbersInListStructured(List.of(12, 9, 23, 32, 11, 20));
+        printEvenNumbersInListStructured(List.of(12, 9, 23, 32, 11, 20));
+        printSquaresOfNumbersInListStructured(List.of(12, 9, 23, 32, 11, 20));
+
+        List<String> courses = List.of(
+                "Spring", "Springboot", "Sanket", "springhh", "nih");
+
+        courses.stream()
+                .filter(course -> course.toLowerCase().contains("spring"))
+                .forEach(System.out::println);
+
+        courses.stream()
+                .filter(course -> course.length() >= 4)
+                .forEach(System.out::println);
+
+        courses.stream()
+                .map(course -> course + " " + course.length())
+                .forEach(System.out::println);
+    }
+
+    private static void printAllNumbersInListStructured(List<Integer> numbers) {
+
+        numbers.stream()
+                .forEach(System.out::println);
+
+    }
+
+    private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+
+        numbers.stream()
+                .filter(number -> number % 2 == 0) // lambda
+                .forEach(System.out::println);
+
+    }
+
+    private static void printSquaresOfNumbersInListStructured(List<Integer> numbers) {
+
+        numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .map(number -> number * number) // lambda
+                .forEach(System.out::println);
+
+    }
+}
